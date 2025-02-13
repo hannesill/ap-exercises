7a3d6d47e5cf4c454420a003aa0663437a982466
hill <hannes.ill@tum.de>
2025-02-13 12:37:04 +0100

    Add multiply and divide functions

--- exercise1.cpp
+++ exercise1.cpp
@@ -5,3 +5,16 @@ int main() {
   
   return 0;
 }
+
+double multiply(double a, double b) {
+  return a * b;
+}
+
+double divide(double a, double b) {
+  if (b == 0) {
+    std::cerr << "ERROR: Trying to divide by zero!\n";
+    return -1;
+  }
+  
+  return a / b;
+}
